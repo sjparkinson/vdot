accc5cde78d61fecfe32ff75424267e610c4442c
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-03-09 18:36:22 +0000

    Update before_deploy.sh

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 perl -i -pe "s/\"0.0.0\"/\"${TRAVIS_TAG#"v"}\"/" Cargo.toml
 
-cargo build --target "$TARGET" --release
+cross build --target "$TARGET" --release
 
 tar -C "target/$TARGET/release/" -czf "target/vdot-$TRAVIS_TAG-$TARGET.tar.gz" vdot
