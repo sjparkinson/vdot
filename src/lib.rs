4f57051ac06eeb25bde99fb809de7a3905216a37
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-03-10 18:02:18 +0000

    feat: add loggerv crate for better logging
    Adds a '--verbose' flag to the command line options.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate failure;
+#[macro_use]
+extern crate log;
 
 use directories::UserDirs;
 use docopt::ArgvMap;
@@ -10,6 +12,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufWriter;
+use std::path::Path;
 use std::{env, fs};
 use url::Url;
 
@@ -35,14 +38,14 @@ impl Config {
         };
 
         let address = match env::var("VAULT_ADDR") {
-            Ok(addr) => addr,
+            Ok(addr) => addr.trim_end_matches('/').to_string(),
             Err(_) => {
                 return Err(format_err!("the $VAULT_ADDR environment variable must be set, e.g. `export VAULT_ADDR=https://vault.example.com`"))
             }
         };
 
-        let paths = args.get_vec("<path>");
-        let paths = paths.into_iter().map(String::from).collect();
+        let paths = args.get_vec("<key>");
+        let paths: Vec<String> = paths.into_iter().map(String::from).collect();
 
         Ok(Self {
             paths,
@@ -53,12 +56,15 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Error> {
+    // Create a new http client to make use of connec
     let http = reqwest::Client::new();
 
-    let mut vars = EnvironmentVariables::new();
+    let mut vars: HashMap<String, String> = HashMap::new();
 
     for path in config.paths {
         let url = format_vault_url(config.address.as_str(), path.as_str())?;
+
+        debug!("making request to \"{}\"", url);
 
         let req = http
             .get(url)
@@ -77,10 +83,18 @@ pub fn run(config: Config) -> Result<(), Error> {
         let resp: Value = resp.json()?;
 
         let data = resp["data"].as_object().unwrap();
+        let data = data.into_iter()
+            .map(|(name, value)| {
+                (name.to_string(), String::from(value.as_str().unwrap()))
+            });
 
         for (name, value) in data {
-            vars.insert(name.to_string(), value.to_string());
+            vars.insert(name, value);
         }
+    }
+
+    if Path::new(".env").is_file() {
+        warn!("overwriting existing .env file");
     }
 
     let file = File::create(".env")?;
@@ -90,14 +104,19 @@ pub fn run(config: Config) -> Result<(), Error> {
 
     save_environment_variables(vars, &mut buf)?;
 
-    println!("Saved {} environment variables to .env", count);
+    info!(
+        "saved {} environment {} to .env",
+        count,
+        if count == 1 { "variable" } else { "variables" }
+    );
 
     Ok(())
 }
 
-type EnvironmentVariables = HashMap<String, String>;
-
-fn save_environment_variables(variables: EnvironmentVariables, w: &mut Write) -> Result<(), Error> {
+fn save_environment_variables<I>(
+    variables: I,
+    w: &mut Write,
+) -> Result<(), Error> where I:IntoIterator<Item = (String, String)> {
     for (variable, value) in variables {
         writeln!(w, "{}={}", variable, value)?;
     }
@@ -107,6 +126,11 @@ fn save_environment_variables(variables: EnvironmentVariables, w: &mut Write) ->
 
 fn format_vault_url(address: &str, path: &str) -> Result<Url, Error> {
     let url = Url::parse(address)?;
+
+    if (url.scheme() != "http" && url.scheme() != "https") || !url.has_authority() {
+        return Err(format_err!("only http and https schemes are allowed in VAULT_ADDR"));
+    }
+
     let url = url.join("v1/")?;
     let url = url.join(path)?;
 
@@ -116,29 +140,88 @@ fn format_vault_url(address: &str, path: &str) -> Result<Url, Error> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::BTreeMap;
 
     #[test]
     fn test_save_environment_variables_formats_for_dotenv() {
-        let mut vars = EnvironmentVariables::new();
+        // Using BTreeMap to get a consistent order.
+        let mut vars: BTreeMap<String, String> = BTreeMap::new();
+        vars.insert(String::from("fizz"), String::from("buzz"));
         vars.insert(String::from("foo"), String::from("bar"));
 
         let mut dotenv = Vec::new();
 
         save_environment_variables(vars, &mut dotenv).unwrap();
 
-        assert_eq!(dotenv, b"foo=bar\n")
+        assert_eq!(String::from_utf8(dotenv).unwrap(), String::from("fizz=buzz\nfoo=bar\n"));
     }
 
     #[test]
     fn test_format_vault_url_formats_to_v1_api() {
         assert_eq!(
+            format_vault_url("http://vault.example.com", "secret/foo-bar").unwrap(),
+            Url::parse("http://vault.example.com/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
+            format_vault_url("http://vault.example.com", "secret/foo-bar").unwrap(),
+            Url::parse("http://vault.example.com/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
             format_vault_url("https://vault.example.com", "secret/foo-bar").unwrap(),
             Url::parse("https://vault.example.com/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
+            format_vault_url("https://vault.example.com/", "secret/foo-bar").unwrap(),
+            Url::parse("https://vault.example.com/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
+            format_vault_url("http://127.0.0.1", "secret/foo-bar").unwrap(),
+            Url::parse("http://127.0.0.1/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
+            format_vault_url("http://127.0.0.1/", "secret/foo-bar").unwrap(),
+            Url::parse("http://127.0.0.1/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
+            format_vault_url("https://127.0.0.1", "secret/foo-bar").unwrap(),
+            Url::parse("https://127.0.0.1/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
+            format_vault_url("https://127.0.0.1/", "secret/foo-bar").unwrap(),
+            Url::parse("https://127.0.0.1/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
+            format_vault_url("http://[::1]", "secret/foo-bar").unwrap(),
+            Url::parse("http://[::1]/v1/secret/foo-bar").unwrap()
+        );
+
+        assert_eq!(
+            format_vault_url("http://[::1]/", "secret/foo-bar").unwrap(),
+            Url::parse("http://[::1]/v1/secret/foo-bar").unwrap()
         );
     }
 
     #[test]
     fn test_format_vault_url_errors_on_bad_address() {
-        assert!(format_vault_url("foo-bar", "fizz-buzz").is_err());
+        assert!(format_vault_url("host-with-no-scheme", "secret/fizz-buzz").is_err());
+        assert!(format_vault_url("https://", "secret/fizz-buzz").is_err());
+        assert!(format_vault_url("http//localhost", "secret/fizz-buzz").is_err());
+
+        // Accepted by `Url::parse` but not valid.
+        // See https://stackoverflow.com/questions/5568569/url-containing-the-scheme-but-not-the-authority-hostname.
+        assert!(format_vault_url("http:localhost", "secret/fizz-buzz").is_err());
+
+        // Only accept http or https.
+        assert!(format_vault_url("data:text/plain", "secret/fizz-buzz").is_err());
+        assert!(format_vault_url("ftp://localhost", "secret/fizz-buzz").is_err());
+        assert!(format_vault_url("unix://localhost", "secret/fizz-buzz").is_err());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#[macro_use]
+extern crate log;
 extern crate directories;
 extern crate docopt;
 extern crate failure;
+extern crate loggerv;
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
@@ -8,6 +11,8 @@ extern crate url;
 extern crate vdot;
 
 use docopt::Docopt;
+use failure::Error;
+use log::Level;
 use std::process;
 use vdot::Config;
 
@@ -17,13 +22,14 @@ vdot
 Create your .env files using Vault.
 
 Usage:
-  vdot <path>...
+  vdot [-v] <key>...
   vdot (-h | --help)
   vdot --version
 
 Options:
-  -h --help     Show this message.
-  --version     Show the version of this program.
+  -h --help      Show this message.
+  --version      Show the version of this program.
+  -v, --verbose  Use verbose output.
 ";
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -33,13 +39,23 @@ fn main() {
         .and_then(|d| d.version(Some(VERSION.to_string())).parse())
         .unwrap_or_else(|e| e.exit());
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Error: {}", err);
-        process::exit(1);
-    });
+    if args.get_bool("--verbose") {
+        loggerv::init_with_level(Level::Debug).unwrap();
+    } else {
+        loggerv::init_with_level(Level::Info).unwrap();
+    }
+
+    let config = match Config::new(&args) {
+        Ok(config) => config,
+        Err(err) => return error(&err),
+    };
 
     if let Err(err) = vdot::run(config) {
-        eprintln!("Error: {}", err);
-        process::exit(1);
+        error(&err);
     }
+}
+
+fn error(err: &Error) {
+    error!("{}", err);
+    process::exit(1);
 }
