865a42c5d124be3f22af0abcee5520747b292fdd
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-03-18 22:25:46 +0000

    refactor: delete half the damn code
    It turns out StructOpt supports all the clap arguments, so make heavy use of those rather than custom logic.

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,6 @@ fn main() {
         _ => logger::init(Level::Trace),
     }
 
-    // We have a valid Args instance, we can run.
-    if args.is_valid() {
-        return run(args);
-    }
-
-    // We don't have the Vault configuration from the command line,
-    // try and get it from the environment.
-    let args = Args::from_env(args).unwrap_or_else(|err| {
-        error!("{}", err);
-        process::exit(exitcode::USAGE);
-    });
-
-    run(args)
-}
-
-fn run(args: Args) {
     if let Err(err) = vdot::run(args) {
         error!("{}", err);
         process::exit(1);
