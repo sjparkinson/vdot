0f6ce04ea4ece908d63ce4db6b264442a963c9d2
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-03-29 10:15:29 +0000

    feat: include README.md and LICENSE in the release artifacts

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -2,8 +2,13 @@
 
 set -eo pipefail
 
+# Update the crates version in Cargo.toml from the git tag
 perl -i -pe "s/\"0.0.0\"/\"${TRAVIS_TAG#"v"}\"/" Cargo.toml
 
 cargo build --target "$TARGET" --release
 
-tar -C "target/$TARGET/release/" -czf "target/vdot-$TRAVIS_TAG-$TARGET.tar.gz" vdot
+# Copy into the release any files to be packaged with the executable
+cp README.md LICENSE "target/$TARGET/release/"
+
+# Package up the release into a .tar.gz
+tar -C "target/$TARGET/release/" -czf "target/vdot-$TRAVIS_TAG-$TARGET.tar.gz" vdot README.md LICENSE
