e84feb5e2beac76bdf7cab438c71fd863e847ded
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-03-29 10:14:35 +0000

    chore(ci): don't use cross to build the release

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eo pipefail
 
 perl -i -pe "s/\"0.0.0\"/\"${TRAVIS_TAG#"v"}\"/" Cargo.toml
 
-cross build --target "$TARGET" --release
+cargo build --target "$TARGET" --release
 
 tar -C "target/$TARGET/release/" -czf "target/vdot-$TRAVIS_TAG-$TARGET.tar.gz" vdot
