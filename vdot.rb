ecb90cf25aacd5201d33a307633eec7020b57886
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-03-31 19:48:27 +0100

    chore(brew): upgrade brew formula to 0.3.7

--- vdot.rb
+++ vdot.rb
@@ -1,6 +1,6 @@
-# This is a formula for enabling vdot to be installed with Homebrew.
+# This is a Homebrew formula for vdot.
 #
-# You can install vdot on macOS with `brew` by running:
+# You can install vdot with `brew` by running:
 #
 # ```
 # brew tap sjparkinson/vdot https://github.com/sjparkinson/vdot
@@ -14,11 +14,11 @@ class Vdot < Formula
   homepage "https://github.com/sjparkinson/vdot"
 
   if OS.mac?
-    url "https://github.com/sjparkinson/vdot/releases/download/v0.3.6/vdot-v0.3.6-x86_64-apple-darwin.tar.gz"
-    sha256 "361b5f0595735454724577d3c4553c697dd7fed4d7e0573fd387273b1ae2f587"
+    url "https://github.com/sjparkinson/vdot/releases/download/v0.3.7/vdot-v0.3.7-x86_64-apple-darwin.tar.gz"
+    sha256 "d27cbe0401311ef9c8f722e1cd9a6cc5638f0bc16890e388e13a70dcfec1b2de"
   elsif OS.linux?
-    url "https://github.com/sjparkinson/vdot/releases/download/v0.3.6/vdot-v0.3.6-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "6ac26e727928efe3ae33b556d83e3939faf7d5a590e25a3bf47ea5ec86b3ece8"
+    url "https://github.com/sjparkinson/vdot/releases/download/v0.3.7/vdot-v0.3.7-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "13d1301fbf544784c198a0b9f6e43c942383664857b030ff1810785448916f10"
   end
 
   def install
