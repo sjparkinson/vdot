0438b78b2f18dbfa6cb0abe8184a56e46bbcf757
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-10-22 21:25:40 +0100

    Include rust profile

--- vdot.rb
+++ vdot.rb
@@ -12,12 +12,13 @@
 class Vdot < Formula
   desc "Create your .env files and start processes using Vault"
   homepage "https://github.com/sjparkinson/vdot"
+  version "0.3.7"
 
   if OS.mac?
-    url "https://github.com/sjparkinson/vdot/releases/download/v0.3.7/vdot-v0.3.7-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/sjparkinson/vdot/releases/download/v#{version}/vdot-v#{version}-x86_64-apple-darwin.zip"
     sha256 "d27cbe0401311ef9c8f722e1cd9a6cc5638f0bc16890e388e13a70dcfec1b2de"
   elsif OS.linux?
-    url "https://github.com/sjparkinson/vdot/releases/download/v0.3.7/vdot-v0.3.7-x86_64-unknown-linux-musl.tar.gz"
+    url "https://github.com/sjparkinson/vdot/releases/download/v#{version}/vdot-v#{version}-x86_64-unknown-linux-musl.zip"
     sha256 "13d1301fbf544784c198a0b9f6e43c942383664857b030ff1810785448916f10"
   end
 
