51879a8362421fd1940e9c5e2de351b9c2e90051
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-10-22 22:09:04 +0100

    Update vdot.rb and update version number in build

--- vdot.rb
+++ vdot.rb
@@ -12,14 +12,14 @@
 class Vdot < Formula
   desc "Create your .env files and start processes using Vault"
   homepage "https://github.com/sjparkinson/vdot"
-  version "0.3.7"
+  version "0.3.21"
 
   if OS.mac?
     url "https://github.com/sjparkinson/vdot/releases/download/v#{version}/vdot-v#{version}-x86_64-apple-darwin.zip"
-    sha256 "d27cbe0401311ef9c8f722e1cd9a6cc5638f0bc16890e388e13a70dcfec1b2de"
+    sha256 "644c9d210cf36ed671275e0a5c3e8aa4d73a280589c5c3715b985e76ff554f9d"
   elsif OS.linux?
     url "https://github.com/sjparkinson/vdot/releases/download/v#{version}/vdot-v#{version}-x86_64-unknown-linux-musl.zip"
-    sha256 "13d1301fbf544784c198a0b9f6e43c942383664857b030ff1810785448916f10"
+    sha256 "9be5063359384a39353dabfd787bf2875dbcdd16b6cd6e65efa827ca6b629852"
   end
 
   def install
