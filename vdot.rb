61660173369597e727b30c315d1dcefe42794fd3
Samuel Parkinson <sam.james.parkinson@gmail.com>
2019-10-23 17:39:41 +0100

    chore(brew): update vdot.rb to v0.4.0

--- vdot.rb
+++ vdot.rb
@@ -12,14 +12,14 @@
 class Vdot < Formula
   desc "Create your .env files and start processes using Vault"
   homepage "https://github.com/sjparkinson/vdot"
-  version "0.3.23"
+  version "0.4.0"
 
   if OS.mac?
     url "https://github.com/sjparkinson/vdot/releases/download/v#{version}/vdot-v#{version}-x86_64-apple-darwin.zip"
-    sha256 "f736fc5d13593032db4e11d9e7038eaf2d880065fb63aa58cec1c3a5547767d1"
+    sha256 "45717b92af2114eeeb98ea1e56fb15f8e8a904210510f8eebfbbf2abf1a1b55b"
   elsif OS.linux?
     url "https://github.com/sjparkinson/vdot/releases/download/v#{version}/vdot-v#{version}-x86_64-unknown-linux-musl.zip"
-    sha256 "52b144355626c5301d56c94ab0ce013889edf497fd3bfa7c58d73ce8df1ec6e5"
+    sha256 "f3e3eed05547e01b272682d6b0f714661bf7d484fd3c07221e1ee7e19d763a28"
   end
 
   def install
